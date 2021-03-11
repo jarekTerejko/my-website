a9e77c143a590a873a6b6cdf5bfe59cdf8741a4f
jarekTerejko <j.terejko@gmail.com>
2021-03-11 07:35:08 +0100

    add new projects

--- src/data.js
+++ src/data.js
@@ -2,26 +2,56 @@ import Manage1 from "./images/portfolio/desktop/image-portfolio-manage.jpg";
 import Bookmark1 from "./images/portfolio/desktop/image-portfolio-bookmark.jpg";
 import Fylo1 from "./images/portfolio/desktop/image-portfolio-fylo.jpg";
 import Insure1 from "./images/portfolio/desktop/image-portfolio-insure.jpg";
+import Photosnap1 from "./images/portfolio/desktop/image-portfolio-photosnap.jpg";
+import MoviesList1 from "./images/portfolio/desktop/image-portfolio-movies.jpg";
+import PayApi1 from "./images/portfolio/desktop/image-portfolio-payapi.jpg";
+import Todo1 from "./images/portfolio/desktop/image-portfolio-todo.jpg";
+import Clock1 from "./images/portfolio/desktop/image-portfolio-clock.jpg";
 import Manage2 from "./images/portfolio/tablet/image-portfolio-manage.jpg";
 import Bookmark2 from "./images/portfolio/tablet/image-portfolio-bookmark.jpg";
 import Fylo2 from "./images/portfolio/tablet/image-portfolio-fylo.jpg";
 import Insure2 from "./images/portfolio/tablet/image-portfolio-insure.jpg";
+import Photosnap2 from "./images/portfolio/tablet/image-portfolio-photosnap.jpg";
+import MoviesList2 from "./images/portfolio/tablet/image-portfolio-movies.jpg";
+import PayApi2 from "./images/portfolio/tablet/image-portfolio-payapi.jpg";
+import Todo2 from "./images/portfolio/tablet/image-portfolio-todo.jpg";
+import Clock2 from "./images/portfolio/tablet/image-portfolio-clock.jpg";
 import Manage3 from "./images/portfolio/mobile/image-portfolio-manage.jpg";
 import Bookmark3 from "./images/portfolio/mobile/image-portfolio-bookmark.jpg";
 import Fylo3 from "./images/portfolio/mobile/image-portfolio-fylo.jpg";
 import Insure3 from "./images/portfolio/mobile/image-portfolio-insure.jpg";
+import Photosnap3 from "./images/portfolio/mobile/image-portfolio-photosnap.jpg";
+import MoviesList3 from "./images/portfolio/mobile/image-portfolio-movies.jpg";
+import PayApi3 from "./images/portfolio/mobile/image-portfolio-payapi.jpg";
+import Todo3 from "./images/portfolio/mobile/image-portfolio-todo.jpg";
+import Clock3 from "./images/portfolio/mobile/image-portfolio-clock.jpg";
 import Manage4 from "./images/project/desktop/image-manage-hero.jpg";
 import Bookmark4 from "./images/project/desktop/image-bookmark-hero.jpg";
 import Fylo4 from "./images/project/desktop/image-fylo-hero.jpg";
 import Insure4 from "./images/project/desktop/image-insure-hero.jpg";
+import Photosnap4 from "./images/project/desktop/image-photosnap-hero.jpg";
+import MoviesList4 from "./images/project/desktop/image-movies-hero.jpg";
+import PayApi4 from "./images/project/desktop/image-payapi-hero.jpg";
+import Todo4 from "./images/project/desktop/image-todo-hero.jpg";
+import Clock4 from "./images/project/desktop/image-clock-hero.jpg";
 import Manage5 from "./images/project/desktop/image-manage-preview-1.jpg";
 import Bookmark5 from "./images/project/desktop/image-bookmark-preview-1.jpg";
 import Fylo5 from "./images/project/desktop/image-fylo-preview-1.jpg";
 import Insure5 from "./images/project/desktop/image-insure-preview-1.jpg";
+import Photosnap5 from "./images/project/desktop/image-photosnap-preview-1.jpg";
+import MoviesList5 from "./images/project/desktop/image-movies-preview-1.jpg";
+import PayApi5 from "./images/project/desktop/image-payapi-preview-1.jpg";
+import Todo5 from "./images/project/desktop/image-todo-preview-1.jpg";
+import Clock5 from "./images/project/desktop/image-clock-preview-1.jpg";
 import Manage6 from "./images/project/desktop/image-manage-preview-2.jpg";
 import Bookmark6 from "./images/project/desktop/image-bookmark-preview-2.jpg";
 import Fylo6 from "./images/project/desktop/image-fylo-preview-2.jpg";
 import Insure6 from "./images/project/desktop/image-insure-preview-2.jpg";
+import Photosnap6 from "./images/project/desktop/image-photosnap-preview-2.jpg";
+import MoviesList6 from "./images/project/desktop/image-movies-preview-2.jpg";
+import PayApi6 from "./images/project/desktop/image-payapi-preview-2.jpg";
+import Todo6 from "./images/project/desktop/image-todo-preview-2.jpg";
+import Clock6 from "./images/project/desktop/image-clock-preview-2.jpg";
 
 export const dataProjects = [
   {
@@ -41,7 +71,7 @@ export const dataProjects = [
       "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
     siteUrl: "https://manage-landing-terejko.netlify.app",
     gitHubUrl: "https://github.com/jarekTerejko/manage-landing-page",
-    languages: ["html", "css", "js"],
+    languages: ["html", "css", "JavaScript"],
     tools: ["sass"],
   },
   {
@@ -61,29 +91,29 @@ export const dataProjects = [
       "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
     siteUrl: "https://bookmark-landing-terejko.netlify.app",
     gitHubUrl: "https://github.com/jarekTerejko/bookmark-landing-page",
-    languages: ["html", "css", "js"],
+    languages: ["html", "css", "JavaScript"],
     tools: ["sass"],
   },
-  {
-    id: 3,
-    images: {
-      img1: Insure1,
-      img2: Insure2,
-      img3: Insure3,
-      img4: Insure4,
-      img5: Insure5,
-      img6: Insure6,
-    },
-    title: "Insure",
-    descriptionShort:
-      "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
-    descriptionLong:
-      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
-    siteUrl: "https://insure-landing-page-terejko.netlify.app",
-    gitHubUrl: "https://github.com/jarekTerejko/insure-landing-page",
-    languages: ["html", "css", "js"],
-    tools: ["sass"],
-  },
+  // {
+  //   id: 3,
+  //   images: {
+  //     img1: Insure1,
+  //     img2: Insure2,
+  //     img3: Insure3,
+  //     img4: Insure4,
+  //     img5: Insure5,
+  //     img6: Insure6,
+  //   },
+  //   title: "Insure",
+  //   descriptionShort:
+  //     "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
+  //   descriptionLong:
+  //     "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+  //   siteUrl: "https://insure-landing-page-terejko.netlify.app",
+  //   gitHubUrl: "https://github.com/jarekTerejko/insure-landing-page",
+  //   languages: ["html", "css", "JavaScript"],
+  //   tools: ["sass"],
+  // },
   {
     id: 4,
     images: {
@@ -101,7 +131,108 @@ export const dataProjects = [
       "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
     siteUrl: "https://fylo-landing-page-terejko.netlify.app",
     gitHubUrl: "https://github.com/jarekTerejko/fylo-dark-theme-landing-page",
-    languages: ["html", "css", "js"],
+    languages: ["html", "css", "JavaScript"],
     tools: ["sass"],
+  },
+  {
+    id: 5,
+    images: {
+      img1: Photosnap1,
+      img2: Photosnap2,
+      img3: Photosnap3,
+      img4: Photosnap4,
+      img5: Photosnap5,
+      img6: Photosnap6,
+    },
+    title: "Photosnap",
+    descriptionShort:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    descriptionLong:
+      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    siteUrl: "https://photosnap-terejko.netlify.app",
+    gitHubUrl: "https://github.com/jarekTerejko/fem-photosnap",
+    languages: ["html", "css", "JavaScript"],
+    tools: ["sass"],
+  },
+  {
+    id: 6,
+    images: {
+      img1: MoviesList1,
+      img2: MoviesList2,
+      img3: MoviesList3,
+      img4: MoviesList4,
+      img5: MoviesList5,
+      img6: MoviesList6,
+    },
+    title: "Movie Lists",
+    descriptionShort:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    descriptionLong:
+      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    siteUrl: "https://movie-lists-terejko.netlify.app",
+    gitHubUrl: "https://github.com/jarekTerejko/movie-lists-react",
+    languages: ["JavaScript"],
+    tools: ["react", "styled components", "API"],
+  },
+  {
+    id: 7,
+    images: {
+      img1: PayApi1,
+      img2: PayApi2,
+      img3: PayApi3,
+      img4: PayApi4,
+      img5: PayApi5,
+      img6: PayApi6,
+    },
+    title: "Photosnap",
+    descriptionShort:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    descriptionLong:
+      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    siteUrl: "https://pay-api-terejko.netlify.app",
+    gitHubUrl: "https://github.com/jarekTerejko/fem-pay-api",
+    languages: ["JavaScript"],
+    tools: ["react", "styled components"],
+  },
+  {
+    id: 8,
+    images: {
+      img1: Todo1,
+      img2: Todo2,
+      img3: Todo3,
+      img4: Todo4,
+      img5: Todo5,
+      img6: Todo6,
+    },
+    title: "Todo",
+    descriptionShort:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    descriptionLong:
+      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    siteUrl: "https://todo-app-terejko.netlify.app",
+    gitHubUrl:
+      "https://github.com/jarekTerejko/fem-challenges-solutions/tree/main/todo-app-main",
+    languages: ["JavaScript"],
+    tools: ["react", "styled components"],
+  },
+  {
+    id: 9,
+    images: {
+      img1: Clock1,
+      img2: Clock2,
+      img3: Clock3,
+      img4: Clock4,
+      img5: Clock5,
+      img6: Clock6,
+    },
+    title: "Clock",
+    descriptionShort:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    descriptionLong:
+      "This project was a front-end challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    siteUrl: "https://clock-app-terejko.netlify.app",
+    gitHubUrl: "https://github.com/jarekTerejko/clock-api-ip-app",
+    languages: ["JavaScript"],
+    tools: ["react", "styled components", "API"],
   },
 ];
