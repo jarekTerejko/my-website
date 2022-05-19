c8b8d3e823c3ca01f0414087ee93f7caab1f590d
jarekTerejko <j.terejko@gmail.com>
2022-05-19 22:16:02 +0200

    add animation to NotFound page

--- src/components/NotFound/NotFoundElements.js
+++ src/components/NotFound/NotFoundElements.js
@@ -1,12 +1,21 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const NotFoundWrapper = styled.div`
   min-height: 100vh;
   width: 100%;
+  margin-bottom: 6rem;
+`;
+
+export const NotFoundContent = styled.div`
+  margin-top: 16rem;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 550px) {
+    margin-top: 10rem;
+  }
 `;
 
 export const NotFoundHeading = styled.h1`
@@ -14,4 +23,8 @@ export const NotFoundHeading = styled.h1`
   font-size: 5rem;
   max-width: 50rem;
   text-align: center;
+
+  @media screen and (max-width: 550px) {
+    font-size: 3rem;
+  }
 `;
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,12 +1,49 @@
-const { LinkR } = require("../Link/LinkElement");
-const { NotFoundWrapper, NotFoundHeading } = require("./NotFoundElements");
+import { motion } from "framer-motion";
+import Lottie from "lottie-web";
+import { useEffect } from "react";
+import NotFoundAnimation from "../../images/404/42969-404-error-cat.json";
+import { WrapperEl } from "../Wrapper/WrapperElement";
+import { LinkR } from "../Link/LinkElement";
+import {
+  NotFoundWrapper,
+  NotFoundHeading,
+  NotFoundContent,
+} from "../NotFound/NotFoundElements";
 
 const NotFound = () => {
+  useEffect(() => {
+    const animation = Lottie.loadAnimation({
+      container: document.querySelector(".animation-container"),
+      animationData: NotFoundAnimation,
+      loop: false,
+    });
+
+    return () => {
+      animation.destroy();
+    };
+  }, []);
+
   return (
     <>
       <NotFoundWrapper>
-        <NotFoundHeading>404 - Taka strona nie istnieje</NotFoundHeading>
-        <LinkR to="/">Home</LinkR>
+        <WrapperEl>
+          <NotFoundContent>
+            <div className="animation-container"></div>
+            <motion.div
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 2 }}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                opacity: 0,
+              }}
+            >
+              <NotFoundHeading>Taka strona nie istnieje</NotFoundHeading>
+              <LinkR to="/">Start</LinkR>
+            </motion.div>
+          </NotFoundContent>
+        </WrapperEl>
       </NotFoundWrapper>
     </>
   );
